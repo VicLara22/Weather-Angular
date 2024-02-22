38c41f4bf764786527f462189e6cb755b696a70a
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2024-02-22 18:12:37 +0100

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  moduleNameMapper: {
-    '@env/(.*)': '<rootDir>/src/environments/$1',
-  },
-  preset: 'jest-preset-angular',
-    globalSetup: 'jest-preset-angular/global-setup',
-    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-};
